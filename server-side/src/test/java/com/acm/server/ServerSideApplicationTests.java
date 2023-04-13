7f191a8a0371268c40ba3decc195fdf28208627d
mh-zeynal <mh.zeynal@gmail.com>
2023-04-13 22:45:16 +0430

    feat: initialize project

---
+++ server-side/src/test/java/com/acm/server/ServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acm.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
