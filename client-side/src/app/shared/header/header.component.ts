d0fbebba0b69eff444e1e5bf644860d983bf77d2
mh-zeynal <mh.zeynal@gmail.com>
2023-04-24 16:58:27 +0430

    feat: redesign header component for desktop devices

--- client-side/src/app/shared/header/header.component.ts
+++ client-side/src/app/shared/header/header.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component, HostListener, Input, OnInit} from '@angular/co
 import {HeaderOptionClass} from "../enums/header-option-class";
 import {Router} from "@angular/router";
 import {fadeInAnimation} from "../animations/fade-animations";
+import {Platform} from "@angular/cdk/platform";
 
 @Component({
   selector: 'acpc-header',
@@ -11,13 +12,18 @@ import {fadeInAnimation} from "../animations/fade-animations";
 })
 export class HeaderComponent implements OnInit, AfterViewInit {
 
+  isDesktop !: boolean;
   isMenuOpen = false;
   menuOptions = ['Home', 'About', 'Register',
-    'Timeline', 'Contact Us', 'Past Results',
-    'Registration Status']
+    'Timeline', 'Contact Us', 'Contests Archive',
+    'Reg. Status']
   @Input() menuOptionColor !: HeaderOptionClass;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private platform: Platform) {
+    this.isDesktop = (!this.platform.ANDROID && !this.platform.IOS);
+    if (this.isDesktop)
+      this.menuOptions.splice(this.menuOptions.indexOf("Register"), 1);
+  }
 
   ngOnInit(): void {
   }
