88ff7b10592d2805717938211ccea85561bda4e4
blue_farid <blue_farid@hotmail.com>
2023-07-17 16:27:38 +0330

    Revert "feat: add mappers for byte arrays"
    This reverts commit a3d14308fc8bcf2d7975082e4638c92784253181.

--- server-side/src/main/java/com/acm/server/domain/picture/Picture.java
+++ server-side/src/main/java/com/acm/server/domain/picture/Picture.java
@@ -3,13 +3,11 @@ package com.acm.server.domain.picture;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.Accessors;
 
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @Entity
 @Getter
 @Setter
-@Accessors(chain = true)
 public abstract class Picture {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
