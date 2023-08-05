1bb510472c896ae6cefd6ae141e3df5cc65cbfec
mh-zeynal <mh.zeynal@gmail.com>
2023-08-05 18:52:22 +0330

    refactor: move header to app component

--- client-side/src/app/shared/components/header/header.component.ts
+++ client-side/src/app/shared/components/header/header.component.ts
@@ -1,41 +1,70 @@
-import {Component, Input, TemplateRef, ViewChild} from '@angular/core';
-import {Router} from "@angular/router";
+import {AfterViewInit, Component, TemplateRef, ViewChild} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
 import {PlatformService} from "../../services/platform.service";
 import {MatBottomSheet, MatBottomSheetRef} from "@angular/material/bottom-sheet";
+import {OverlayContainer} from "@angular/cdk/overlay";
 
 @Component({
   selector: 'acpc-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
 
   isDesktop !: boolean;
   isMenuOpen = false;
   menuOptions = ['Home', 'About', 'Register',
     'Timeline', 'Contact Us', 'Reg. Status', 'Contests Archive',]
-  @Input() menuOptionColor !: 'dark-text' | 'light-text';
+  menuColor !: 'dark-text' | 'light-text';
 
   bottomSheetRef!: MatBottomSheetRef<any>;
   @ViewChild('bottomSheet') bottomSheetTemplate!: TemplateRef<any>;
 
-  constructor(private router: Router, private bottomSheet: MatBottomSheet, private platform: PlatformService) {
+  constructor(private router: Router, private bottomSheet: MatBottomSheet,
+              private platform: PlatformService,
+              private overlayContainer: OverlayContainer) {
     this.isDesktop = platform.isOnDesktopDevice();
     if (this.isDesktop) {
       this.menuOptions.splice(this.menuOptions.indexOf("Register"), 1);
       this.menuOptions.splice(this.menuOptions.indexOf("Home"), 1);
     }
-    if (!this.menuOptionColor)
-      this.menuOptionColor = 'dark-text';
+  }
+
+  ngAfterViewInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentUrl = event.url; // Get the current URL
+        if (currentUrl == '/home')
+          this.menuColor = 'light-text';
+        else
+          this.menuColor = 'dark-text';
+      }
+    });
   }
 
   openMenu() {
     this.isMenuOpen = !this.isMenuOpen;
 
     this.bottomSheetRef = this.bottomSheet.open(this.bottomSheetTemplate);
-    this.bottomSheetRef.backdropClick().subscribe(e => {
+    this.bottomSheetRef.backdropClick().subscribe(() => {
       this.isMenuOpen = !this.isMenuOpen;
     });
+
+    const containerElement = this.overlayContainer.getContainerElement()
+      .querySelector('.mat-bottom-sheet-container') as HTMLElement;
+
+    let startY: number;
+    containerElement.addEventListener('touchstart', (event) => {
+      startY = event.touches[0].clientY;
+    });
+
+    containerElement.addEventListener('touchmove', (event) => {
+      const deltaY = event.touches[0].clientY - startY;
+      containerElement.style.bottom = `${-1 * deltaY}px`
+      if (deltaY > 300)
+        this.bottomSheetRef.dismiss();
+    });
+
     this.highlightCurrentRouteOption();
   }
 
