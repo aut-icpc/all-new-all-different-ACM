5baa7b8113705c04d3d76b37fddff54319b5df0c
mh-zeynal <mh.zeynal@gmail.com>
2023-08-17 09:09:12 +0330

    feat: set route guard on registration success page

--- client-side/src/app/registration/registration-routing.module.ts
+++ client-side/src/app/registration/registration-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {TeamRegistrationPageComponent} from "./team-registration-page/team-registration-page.component";
 import {RegistrationStatusPageComponent} from "./registration-status-page/registration-status-page.component";
 import {RegistrationSuccessPageComponent} from "./registration-success-page/registration-success-page.component";
+import {TeamSessionDataGuard} from "./team-session-data.guard";
 
 const routes: Routes = [
   {path: 'registration', children: [
       {path: 'register', component: TeamRegistrationPageComponent},
       {path: 'reg-status', component: RegistrationStatusPageComponent},
-      {path: 'success', component: RegistrationSuccessPageComponent}
+      {path: 'success', component: RegistrationSuccessPageComponent, canActivate: [TeamSessionDataGuard]}
     ]
   },
 ];
