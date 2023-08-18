e6986bc78ff2a9ecfe8efd8d3fd2fcd0874eb6d3
mh-zeynal <mh.zeynal@gmail.com>
2023-08-18 18:46:20 +0330

    refactor: remove unused import

--- server-side/src/main/java/com/acm/server/service/impl/ContactUsServiceImpl.java
+++ server-side/src/main/java/com/acm/server/service/impl/ContactUsServiceImpl.java
@@ -1,7 +1,6 @@
 package com.acm.server.service.impl;
 
 import com.acm.server.mapper.ContactUsMapper;
-import com.acm.server.mapper.RegistrationTermsMapper;
 import com.acm.server.model.dto.ContactUsDto;
 import com.acm.server.repository.ContactUsRepository;
 import com.acm.server.service.ContactUsService;
@@ -19,6 +18,13 @@ public class ContactUsServiceImpl implements ContactUsService {
     @Override
     public ContactUsDto getContactUs() {
         try {
+//            var test = new ContactUs();
+//            test.setEmail("ceaut.acpc@gmail.com");
+//            test.setInstagram("https://www.instagram.com/acpc.2023/");
+//            test.setTelegram("https://t.me/acpc_admin");
+//            test.setUniversityCoordinate(new Point(35.70385,51.40833));
+//            test.setPhoneNumber("+989011521493");
+//            repository.save(test);
             return repository.findAll().stream().map(mapper::toDto).toList().get(0);
         } catch (NullPointerException e) {
             log.error("Contact Us table is empty!", e);
