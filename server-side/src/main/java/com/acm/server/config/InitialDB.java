e83bdbdb577e5dc871e91c751521b829b6c9aebc
blue_farid <blue_farid@hotmail.com>
2023-08-24 14:43:11 +0330

    refactor: remove unused initial methods

--- server-side/src/main/java/com/acm/server/config/InitialDB.java
+++ server-side/src/main/java/com/acm/server/config/InitialDB.java
@@ -1,9 +1,6 @@
 package com.acm.server.config;
 
-import com.acm.server.domain.ContactUs;
-import com.acm.server.domain.Point;
 import com.acm.server.domain.file.picture.AboutUsPicture;
-import com.acm.server.repository.ContactUsRepository;
 import com.acm.server.repository.PictureRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -13,31 +10,13 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class InitialDB implements CommandLineRunner {
-    private final ContactUsRepository contactUsRepository;
     private final PictureRepository pictureRepository;
-    private final ContactUsConfiguration contactUsConfiguration;
     private final HostConfiguration hostConfiguration;
 
     @Override
     public void run(String... args) {
-        if (contactUsRepository.count() == 0)
-            initialContactUs();
-
         if (pictureRepository.count(Example.of(new AboutUsPicture())) == 0)
             initialAboutUsPicture();
-    }
-
-    private void initialContactUs() {
-        ContactUs contactUs = new ContactUs()
-                .setInstagram(contactUsConfiguration.getInstagram())
-                .setTelegram(contactUsConfiguration.getTelegram())
-                .setPhoneNumber(contactUsConfiguration.getPhone())
-                .setEmail(contactUsConfiguration.getMail())
-                .setUniversityCoordinate(new Point()
-                        .setX(Double.parseDouble(contactUsConfiguration.getPointX()))
-                        .setY(Double.parseDouble(contactUsConfiguration.getPointY())));
-
-        contactUsRepository.save(contactUs);
     }
 
     private void initialAboutUsPicture() {
