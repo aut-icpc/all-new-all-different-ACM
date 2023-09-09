d0477dc265a211f916bb60f88b843f63f638620c
blue_farid <blue_farid@hotmail.com>
2023-09-09 21:25:14 +0330

    feat: add auth controller

---
+++ server-side/src/main/java/com/acm/server/controller/AuthenticationController.java
@@ -0,0 +1,24 @@
+package com.acm.server.controller;
+
+import com.acm.server.config.Constants;
+import com.acm.server.model.dto.BaseResponseDto;
+import com.acm.server.request.SignInRequest;
+import com.acm.server.response.JwtAuthenticationResponse;
+import com.acm.server.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(Constants.BASE_CONTEST_API_URL + "/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public ResponseEntity<BaseResponseDto<JwtAuthenticationResponse>> login(SignInRequest request) {
+        return ResponseEntity.ok(
+                new BaseResponseDto<>("logged in!", authenticationService.signIn(request))
+        );
+    }
+}
