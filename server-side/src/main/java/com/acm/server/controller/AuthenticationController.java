c0d78c1ab56225467bd3ef787dad107c4e1e809d
blue_farid <blue_farid@hotmail.com>
2023-09-14 11:30:42 +0330

    fix: jwt issue

--- server-side/src/main/java/com/acm/server/controller/AuthenticationController.java
+++ server-side/src/main/java/com/acm/server/controller/AuthenticationController.java
@@ -8,6 +8,7 @@ import com.acm.server.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/login")
-    public ResponseEntity<BaseResponseDto<JwtAuthenticationResponse>> login(SignInRequest request) {
+    public ResponseEntity<BaseResponseDto<JwtAuthenticationResponse>> login(@RequestBody SignInRequest request) {
         return ResponseEntity.ok(
                 new BaseResponseDto<>("logged in!", authenticationService.signIn(request))
         );
