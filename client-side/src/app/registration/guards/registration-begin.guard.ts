dab8f9866606c396782a3fc0ddf4b714b4c5307d
Alireza <alireza.yousefpourm@gmail.com>
2023-09-22 01:43:14 +0330

    chore: Updated angular to 16

--- client-side/src/app/registration/guards/registration-begin.guard.ts
+++ client-side/src/app/registration/guards/registration-begin.guard.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import {Observable} from 'rxjs';
 import {HttpService} from "../../shared/services/http.service";
 import {BaseResponseDto} from "../../shared/interfaces/DTO/baseResponse.dto";
@@ -10,7 +10,7 @@ import {map} from "rxjs/operators";
 @Injectable({
   providedIn: 'root'
 })
-export class RegistrationBeginGuard implements CanActivate {
+export class RegistrationBeginGuard  {
 
   constructor(private http: HttpService, private router: Router) { }
 
