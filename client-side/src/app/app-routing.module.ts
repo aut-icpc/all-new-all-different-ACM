b05df11d557b3bc8595f2b93d30a4f447255c4ac
mh-zeynal <mh.zeynal@gmail.com>
2023-10-07 19:47:57 +0330

    chore: add title to web pages

--- client-side/src/app/app-routing.module.ts
+++ client-side/src/app/app-routing.module.ts
@@ -4,8 +4,14 @@ import {HomePageComponent} from "./home-page/home-page.component";
 import {ContactUsPageComponent} from "./contact-us-page/contact-us-page.component";
 
 const routes: Routes = [
-  {path: 'home', component: HomePageComponent},
-  {path: 'contact-us', component: ContactUsPageComponent},
+  {
+    path: 'home', component: HomePageComponent,
+    title: 'ACPC - Amirkabir University of Technology Collegiate Programming Contest'
+  },
+  {
+    path: 'contact-us', component: ContactUsPageComponent,
+    title: 'Contact us',
+  },
   {path: '', redirectTo: 'home', pathMatch: 'full'},
 ];
 
--- client-side/src/app/registration/registration-routing.module.ts
+++ client-side/src/app/registration/registration-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {TeamRegistrationPageComponent} from "./team-registration-page/team-registration-page.component";
 import {RegistrationStatusPageComponent} from "./registration-status-page/registration-status-page.component";
 import {RegistrationSuccessPageComponent} from "./registration-success-page/registration-success-page.component";
@@ -9,10 +9,25 @@ import {RegistrationBeginGuard} from "./guards/registration-begin.guard";
 
 const routes: Routes = [
   {path: 'registration', children: [
-      {path: 'register', component: TeamRegistrationPageComponent, canActivate: [RegistrationBeginGuard]},
-      {path: 'reg-status', component: RegistrationStatusPageComponent, canActivate: [RegistrationBeginGuard]},
-      {path: 'success', component: RegistrationSuccessPageComponent, canActivate: [TeamSessionDataGuard, RegistrationBeginGuard]},
-      {path: 'coming-soon', component: ComingSoonPageComponent}
+      {
+        path: 'register', component: TeamRegistrationPageComponent,
+        canActivate: [RegistrationBeginGuard],
+        title: 'Team registration',
+      },
+      {
+        path: 'reg-status', component: RegistrationStatusPageComponent,
+        canActivate: [RegistrationBeginGuard],
+        title: 'Registration status'
+      },
+      {
+        path: 'success', component: RegistrationSuccessPageComponent,
+        canActivate: [TeamSessionDataGuard, RegistrationBeginGuard],
+        title: 'Team registration successful'
+      },
+      {
+        path: 'coming-soon', component: ComingSoonPageComponent,
+        title: 'Coming soon'
+      }
     ]
   },
 ];
