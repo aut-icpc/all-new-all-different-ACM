b0e1321c8ca802765e60a67efa411ba2b04e66de
Alireza <alireza.yousefpourm@gmail.com>
2023-10-09 22:43:06 +0330

    chore: Add click-scroll/touch-scroll feature

--- client-side/src/app/shared/components/image-slider/image-slider-container/image-slider-container.component.ts
+++ client-side/src/app/shared/components/image-slider/image-slider-container/image-slider-container.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input} from '@angular/core';
+import {Component, HostListener, Input} from '@angular/core';
+import {PlatformService} from "../../../services/platform.service";
 
 @Component({
   selector: 'acpc-image-slider-container',
@@ -6,25 +7,35 @@ import {Component, Input} from '@angular/core';
   styleUrls: ['./image-slider-container.component.scss']
 })
 export class ImageSliderContainerComponent {
-  // @Input() images: string[] | null = null; FIXME
-  @Input() images: string[] | null = [
-    "https://images.dog.ceo/breeds/sheepdog-shetland/n02105855_15602.jpg",
-    "https://images.dog.ceo/breeds/dingo/n02115641_7222.jpg",
-    "https://images.dog.ceo/breeds/terrier-lakeland/n02095570_4560.jpg",
-    "https://images.dog.ceo/breeds/eskimo/n02109961_2369.jpg",
-    "https://images.dog.ceo/breeds/hound-walker/n02089867_1931.jpg",
-    "https://images.dog.ceo/breeds/setter-irish/n02100877_1062.jpg",
-    "https://images.dog.ceo/breeds/terrier-lakeland/n02095570_4560.jpg",
-    "https://images.dog.ceo/breeds/eskimo/n02109961_2369.jpg",
-    "https://images.dog.ceo/breeds/hound-walker/n02089867_1931.jpg",
-    "https://images.dog.ceo/breeds/setter-irish/n02100877_1062.jpg",
-    "https://images.dog.ceo/breeds/terrier-lakeland/n02095570_4560.jpg",
-    "https://images.dog.ceo/breeds/eskimo/n02109961_2369.jpg",
-    "https://images.dog.ceo/breeds/hound-walker/n02089867_1931.jpg",
-    "https://images.dog.ceo/breeds/setter-irish/n02100877_1062.jpg",
-    "https://images.dog.ceo/breeds/doberman/n02107142_13483.jpg",
-  ]
+  @Input() images: string[] | null = null;
+  // @Input() images: string[] | null = [
+  //   "https://images.dog.ceo/breeds/sheepdog-shetland/n02105855_15602.jpg",
+  //   "https://images.dog.ceo/breeds/dingo/n02115641_7222.jpg",
+  //   "https://images.dog.ceo/breeds/terrier-lakeland/n02095570_4560.jpg",
+  //   "https://images.dog.ceo/breeds/eskimo/n02109961_2369.jpg",
+  //   "https://images.dog.ceo/breeds/hound-walker/n02089867_1931.jpg",
+  //   "https://images.dog.ceo/breeds/setter-irish/n02100877_1062.jpg",
+  //   "https://images.dog.ceo/breeds/terrier-lakeland/n02095570_4560.jpg",
+  //   "https://images.dog.ceo/breeds/eskimo/n02109961_2369.jpg",
+  //   "https://images.dog.ceo/breeds/hound-walker/n02089867_1931.jpg",
+  //   "https://images.dog.ceo/breeds/setter-irish/n02100877_1062.jpg",
+  //   "https://images.dog.ceo/breeds/terrier-lakeland/n02095570_4560.jpg",
+  //   "https://images.dog.ceo/breeds/eskimo/n02109961_2369.jpg",
+  //   "https://images.dog.ceo/breeds/hound-walker/n02089867_1931.jpg",
+  //   "https://images.dog.ceo/breeds/setter-irish/n02100877_1062.jpg",
+  //   "https://images.dog.ceo/breeds/doberman/n02107142_13483.jpg",
+  // ]
   currentIndex: number = 0
+  isOnDesktop!: boolean
+  isTouched: boolean = false
+  touchPosition: { x: number, y: number } | null = null
+  isDragDisabled: boolean = false
+
+  SCROLL_THRESHOLD: number = 35
+
+  constructor(private platform: PlatformService,) {
+    this.isOnDesktop = platform.isOnDesktopDevice()
+  }
 
   forwardButtonOnClick() {
     if (this.images == null) return
@@ -71,6 +82,112 @@ export class ImageSliderContainerComponent {
       classes += 'selected'
 
     return classes
+  }
+
+  @HostListener('touchstart', ['$event'])
+  touchStartHandler() {
+    if (this.isOnDesktop) return
+
+    const touchEvent = event as TouchEvent
+
+    const touch = touchEvent.touches[0] || touchEvent.changedTouches[0];
+    this.touchPosition = {
+      x: touch.clientX,
+      y: touch.clientY
+    }
+    this.isTouched = true
+
+  }
+
+  @HostListener('touchend')
+  touchEndHandler() {
+    if (this.isOnDesktop) return
+
+    this.isTouched = false
+  }
+
+  @HostListener('touchmove', ['$event'])
+  touchMoveHandler() {
+    if (this.isOnDesktop) return
+    if (!this.isTouched) return
+    if (!this.touchPosition) return
+
+    const touchEvent = event as TouchEvent
+    const touch = touchEvent.touches[0] || touchEvent.changedTouches[0];
+
+    if (this.touchPosition.x - touch.clientX > this.SCROLL_THRESHOLD) {
+      this.forwardButtonOnClick()
+      this.touchPosition = {
+        x: touch.clientX,
+        y: touch.clientY,
+      }
+      this.isTouched = false
+    } else if (touch.clientX - this.touchPosition.x > this.SCROLL_THRESHOLD) {
+      this.backwardsButtonOnClick()
+      this.touchPosition = {
+        x: touch.clientX,
+        y: touch.clientY,
+      }
+      this.isTouched = false
+    }
+  }
+
+  @HostListener('mousedown', ['$event'])
+  mouseDownHandler() {
+    if (!this.isOnDesktop) return
+
+    const mouseEvent = event as MouseEvent
+    this.touchPosition = {
+      x: mouseEvent.clientX,
+      y: mouseEvent.clientY
+    }
+
+    if (!this.isDragDisabled) {
+      const stuff = document.querySelectorAll('.slider-image')
+      if (stuff.length === 0) return
+
+      stuff.forEach(el => {
+        el.addEventListener('dragstart', (e) => {
+          e.preventDefault();
+        })
+      })
+
+      this.isDragDisabled = true;
+    }
+
+    this.isTouched = true
+  }
+
+  @HostListener('mouseup')
+  mouseUpHandler() {
+    if (!this.isOnDesktop) return
+
+    this.isTouched = false
+  }
+
+  @HostListener('mousemove', ['$event'])
+  mouseMoveHandler() {
+    if (!this.isOnDesktop) return
+    if (!this.isTouched) return
+    if (!this.touchPosition) return
+
+    const mouseEvent = event as MouseEvent
+
+    if (this.touchPosition.x - mouseEvent.clientX > this.SCROLL_THRESHOLD) {
+      this.forwardButtonOnClick()
+      this.touchPosition = {
+        x: mouseEvent.clientX,
+        y: mouseEvent.clientY,
+      }
+      this.isTouched = false
+    } else if (mouseEvent.clientX - this.touchPosition.x > this.SCROLL_THRESHOLD) {
+      this.backwardsButtonOnClick()
+      this.touchPosition = {
+        x: mouseEvent.clientX,
+        y: mouseEvent.clientY,
+      }
+      this.isTouched = false
+    }
   }
 
   showContainer() {
