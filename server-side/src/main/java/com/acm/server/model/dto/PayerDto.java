5433051ed73271a189348d90f159761b61949498
blue_farid <blue_farid@hotmail.com>
2023-10-20 14:56:33 +0330

    fix postgres start

--- server-side/src/main/java/com/acm/server/model/dto/PayerDto.java
+++ server-side/src/main/java/com/acm/server/model/dto/PayerDto.java
@@ -1,11 +1,7 @@
 package com.acm.server.model.dto;
-
-<<<<<<< HEAD
-=======
 import lombok.Data;
 
 @Data
->>>>>>> deploy
 public class PayerDto {
     private String firstName;
     private String lastName;
