845e6d36adacc9d1bd70895943ce4f8ac5cfa3c1
mh-zeynal <mh.zeynal@gmail.com>
2023-10-25 14:24:16 +0330

    fix: fix shirt size select list

--- client-side/src/environments/environment.prod.ts
+++ client-side/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   },
   registration: {
     educationLevels: ['Undergraduate(BSc.)', 'Graduate(MSc.)'],
-    shirtSizes: ['Small', 'Medium', 'Large', 'XL', 'XXL'],
+    shirtSizes: ['Small', 'Medium', 'Large', 'XL', 'XXL', 'XXXL'],
     genders: ['male', 'female']
   },
   inputValidators: {
