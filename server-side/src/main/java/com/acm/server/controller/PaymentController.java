90f42b3303721d51b2d6ad70cb588e19cacdaaa5
amirhosseinaghighi <amirhossein.aghighi2@gmail.com>
2024-11-01 22:03:32 +0330

    fix payment verify response

--- server-side/src/main/java/com/acm/server/controller/PaymentController.java
+++ server-side/src/main/java/com/acm/server/controller/PaymentController.java
@@ -3,9 +3,9 @@ package com.acm.server.controller;
 import com.acm.server.config.Constants;
 import com.acm.server.service.PaymentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +17,25 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     @PostMapping("/")
-    public ResponseEntity<String> verify(
+    public ResponseEntity<Void> verify(
             @RequestParam(required = false) String clientrefid,
             @RequestParam(required = false) String refid) {
-        String[] data = clientrefid.split("[\\+\\-]+");
-        paymentService.verify(Long.parseLong(refid.trim()), Long.parseLong(data[0].trim()), Long.parseLong(data[1].trim()));
-        return ResponseEntity.ok("verified");
+        try {
+            String[] data = clientrefid.split("[\\+\\-]+");
+            String code = paymentService.verify(
+                    Long.parseLong(refid.trim()),
+                    Long.parseLong(data[0].trim()),
+                    Long.parseLong(data[1].trim())
+            );
+
+            return ResponseEntity.status(HttpStatus.FOUND)
+                    .header("Location", Constants.BASE_CONTEST_API_URL + "/payment_status/?status=success&code="+code)
+                    .build();
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.FOUND)
+                    .header("Location", Constants.BASE_CONTEST_API_URL + "/payment_status/?status=failed&code="+e.getMessage())
+                    .build();
+        }
     }
 }
