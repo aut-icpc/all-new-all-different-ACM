6bff1b28cbf40a6fd3f4377b5fbe10cc2d89d544
azare242 <azare242@gmail.com>
2024-11-03 01:00:06 +0330

    fix

--- server-side/src/main/java/com/acm/server/controller/PaymentController.java
+++ server-side/src/main/java/com/acm/server/controller/PaymentController.java
@@ -42,7 +42,7 @@ public ResponseEntity<PaymentResponse> verify(@RequestParam String data) {
         Long clientRefPart2 = Long.parseLong(splitData[1].trim());
 
         String code = paymentService.verify(
-            Long.parseLong(paymentRefId),
+            paymentRefId,
             clientRefPart1,
             clientRefPart2
         );
