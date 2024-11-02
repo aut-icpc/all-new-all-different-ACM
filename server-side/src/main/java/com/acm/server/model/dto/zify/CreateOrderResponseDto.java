8d65a8fb61456d02fc63685d25abbdfae0c0346d
azare242 <azare242@gmail.com>
2024-11-02 21:38:35 +0330

    remove unnecesary fields from zify dtos

--- server-side/src/main/java/com/acm/server/model/dto/zify/CreateOrderResponseDto.java
+++ server-side/src/main/java/com/acm/server/model/dto/zify/CreateOrderResponseDto.java
@@ -5,9 +5,9 @@ import lombok.Data;
 @Data
 public class CreateOrderResponseDto {
     private int Amount;
-    private String PayerIdentity;
-    private String PayerName;
-    private String Description;
+    // private String PayerIdentity;
+    // private String PayerName;
+    // private String Description;
     private String ReturnUrl;
     private String ClientRefId;
 }
